7e338f2b77dcb06d1786d7ca7b858182c1c81cbc
Kirsty Simmonds <kirstysimmonds@icloud.com>
2019-02-21 10:59:03 +0100

    Yelp MVP -- unfinished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Destroying all Restaurants'
+
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:  'chinese',
+    phone_number:        '762786871691'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:  'italian',
+    phone_number: '78637648973653'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
